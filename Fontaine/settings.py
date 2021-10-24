f57ee5e9efea0c4d510a7b65da00524b8148fe9a
sam-mugo <keithgety@gmail.com>
2021-10-24 15:34:23 +0300

    attempt 1 key remove

--- Fontaine/settings.py
+++ Fontaine/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
+from dotenv import load_dotenv
 
 from pathlib import Path
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8d#)9=8!gq!nnsdl3!edw453nas_0g*(psudcih0+_qj@_b87$'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
