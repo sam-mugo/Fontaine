19089481aae1aa1bf5bf2df7802d8066fc454684
sam-mugo <keithgety@gmail.com>
2021-10-25 07:14:34 +0300

    wiring templates to views

--- Fontaine/urls.py
+++ Fontaine/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from blog_app.views import frontpage
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', frontpage, name='frontpage'),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def frontpage(request):
+    return render(request, 'core/base.html')
