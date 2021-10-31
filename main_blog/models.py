a1e0bc4a6ef3688e3f15144f2a1c1381ab5bde6c
sam-mugo <keithgety@gmail.com>
2021-10-31 18:52:02 +0300

    make Posts models and apply migrations

--- main_blog/models.py
+++ main_blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.db.models.fields import CharField, SlugField
 
 # Create your models here.
+class Posts(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    intro = models.TextField()
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
