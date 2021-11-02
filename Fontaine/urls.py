9ae00485eceaad080a78515226e7838738e1a94a
sam-mugo <keithgety@gmail.com>
2021-11-02 11:39:52 +0300

    post detail page

--- Fontaine/urls.py
+++ Fontaine/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from blog_app.views import frontpage, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('main_blog.urls')),
     path('', frontpage, name='frontpage'),
-    path('about/', about, name='about'),
 ]
--- main_blog/views.py
+++ main_blog/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from main_blog.models import Posts
 
 # Create your views here.
+def detail(request, slug):
+    post = get_object_or_404(Posts, slug=slug)
+    
+    return render(request, 'blog/detail.html', {'post': post})
