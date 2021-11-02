333ca38c72f7ac193667220a3ca7f0e3b4c631f4
sam-mugo <keithgety@gmail.com>
2021-11-02 11:35:39 +0300

    add reverse ordering so the first post created is the first displayed

--- main_blog/models.py
+++ main_blog/models.py
@@ -8,3 +8,6 @@ class Posts(models.Model):
     intro = models.TextField()
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ('-created_at',)
