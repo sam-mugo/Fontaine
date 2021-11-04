d5ac269d837ea892ab37f337b4448f222ddb7acc
sam-mugo <keithgety@gmail.com>
2021-11-04 21:51:59 +0300

    comment model

--- main_blog/models.py
+++ main_blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 from django.db.models.fields import CharField, SlugField
 
 # Create your models here.
@@ -11,3 +12,12 @@ class Posts(models.Model):
     
     class Meta:
         ordering = ('-created_at',)
+        
+        
+class Comment(models.Model):
+    post = models.ForeignKey(Posts, related_name='comments', on_delete=CASCADE)
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
