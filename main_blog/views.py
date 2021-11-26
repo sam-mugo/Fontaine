6cf107b242a134b3d7e63dfc017850cd65b1f122
sam-mugo <keithgety@gmail.com>
2021-11-26 21:26:33 +0300

    linking comment models to views

--- main_blog/views.py
+++ main_blog/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import get_object_or_404, render
 
-from main_blog.models import Posts
+
+from .forms import CommentForm
+from .models import Posts
 
 # Create your views here.
 def detail(request, slug):
     post = get_object_or_404(Posts, slug=slug)
     
-    return render(request, 'blog/detail.html', {'post': post})
+    form = CommentForm()
+    
+    return render(request, 'blog/detail.html', {'post': post, 'form': form})
