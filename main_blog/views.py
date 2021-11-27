1cc5d81f6f0c7910ef3e090fa89d1cb244a22b81
sam-mugo <keithgety@gmail.com>
2021-11-27 21:51:35 +0300

    comments form

--- main_blog/views.py
+++ main_blog/views.py
@@ -1,13 +1,23 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 
 
 from .forms import CommentForm
-from .models import Posts
+from .models import Comment, Posts
 
 # Create your views here.
 def detail(request, slug):
     post = get_object_or_404(Posts, slug=slug)
     
-    form = CommentForm()
-    
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            
+            return redirect('post_detail', slug=slug)
+    else:
+        form = CommentForm()
+        
     return render(request, 'blog/detail.html', {'post': post, 'form': form})
