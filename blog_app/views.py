b6e40b2c425e7735a1c633ce679edcccbcc3a601
sam-mugo <keithgety@gmail.com>
2021-11-30 13:21:59 +0300

    filter out draft posts

--- blog_app/views.py
+++ blog_app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from main_blog.models import Posts
 
 def frontpage(request):
-    posts = Posts.objects.all()
+    posts = Posts.objects.filter(status=Posts.ACTIVE)
     return render(request, 'core/home.html', {'posts': posts})
 
 def about(request):
