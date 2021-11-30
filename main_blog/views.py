0c64b6b75067005c315e7e63583adcb758a48a3b
sam-mugo <keithgety@gmail.com>
2021-11-30 13:17:26 +0300

    add search functionality that queries the db

--- main_blog/views.py
+++ main_blog/views.py
@@ -1,11 +1,13 @@
+from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
+
 
 
 from .forms import CommentForm
 from .models import Comment, Posts, Category
 
 # Create your views here.
-def detail(request, category_slug, slug):
+def detail(request, category_slug, slug, status=Posts.ACTIVE):
     post = get_object_or_404(Posts, slug=slug)
     
     if request.method == 'POST':
@@ -18,11 +20,20 @@ def detail(request, category_slug, slug):
             
             return redirect('post_detail', slug=slug)
     else:
-        form = CommentForm()
+        form = CommentForm() 
         
     return render(request, 'blog/detail.html', {'post': post, 'form': form})
 
 def category(request, slug):
     category = get_object_or_404(Category, slug=slug)
+    posts = category.posts.filter(status=Posts.ACTIVE)
     
-    return render(request, 'blog/category.html', {'category': category})
+    return render(request, 'blog/category.html', {'category': category, 'posts': posts})
+
+
+def search(request):
+    query = request.GET.get('query', '')
+    
+    posts = Posts.objects.filter(Q(title__icontains=query) | Q(intro__icontains=query) | Q(body__icontains=query))
+    
+    return render(request, 'blog/search.html', {'posts': posts, 'query': query})
