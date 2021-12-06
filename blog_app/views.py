dc5d2687ef864db15abd90144d6cca5836ee70d1
sam-mugo <keithgety@gmail.com>
2021-12-06 23:02:55 +0300

    robots.txt

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,4 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 from main_blog.models import Posts
@@ -8,3 +9,11 @@ def frontpage(request):
 
 def about(request):
     return render(request, 'core/about.html')
+
+
+def robots_txt(request):
+    text = [
+        "User-Agent: *",
+        "Disallow: /admin/",
+    ]
+    return HttpResponse("\n".join(text, contest_type="text/plain"))
