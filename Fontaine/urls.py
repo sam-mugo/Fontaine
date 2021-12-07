69ab29f6cec46e3f8e11d3bfc726a06c3a765ad1
sam-mugo <keithgety@gmail.com>
2021-12-07 22:29:34 +0300

    add robots.txt path to urlpatterns

--- Fontaine/urls.py
+++ Fontaine/urls.py
@@ -22,12 +22,13 @@ from django.urls import path, include
 
 from .sitemaps import CategorySitemap, PostSitemap
 
-from blog_app.views import frontpage, about
+from blog_app.views import frontpage, about, robots_txt
 
 sitemaps = {'category': CategorySitemap, 'post': PostSitemap}
 
 urlpatterns = [
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}),
+    path('robots.txt/', robots_txt, name='robots_txt'),
     path('admin/', admin.site.urls),
     path('', include('main_blog.urls')),
     path('', frontpage, name='frontpage'),
