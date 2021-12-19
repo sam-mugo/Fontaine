9595fe736439fd46eba3e53fd4b2e3a76513762f
sam-mugo <keithgety@gmail.com>
2021-12-19 20:16:16 +0300

    debug = false

--- Fontaine/settings.py
+++ Fontaine/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = ''
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['fontaine.herokuapp.com']
 
